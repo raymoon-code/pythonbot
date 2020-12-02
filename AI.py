294c68c2fe0e0185f22fe813aeac7eb68bee16f8
raymoon-code <66543973+raymoon-code@users.noreply.github.com>
2020-12-02 01:29:25 -0600

    Update AI.py

--- AI.py
+++ AI.py
@@ -454,8 +454,8 @@ async def fight(ctx, user: discord.Member = None, user2: discord.Member = None):
         pfp2 = pfp2.resize((200,200))
         fight.paste(pfp,(81,223))
         fight.paste(pfp2,(703,223))
-        font = ImageFont.truetype('Alice_in_Wonderland_3.ttf', 45)
-        print(font)                        
+        font = ImageFont.truetype('arial.ttf', 45)
+                                
         draw.text((100,440), str(user_name),(251, 232, 255), font=font)
         draw.text((730,440), str(user2_name),(251, 232, 255), font=font)
     elif r == 'fight1.jpg':
@@ -465,8 +465,8 @@ async def fight(ctx, user: discord.Member = None, user2: discord.Member = None):
         fight.paste(pfp2,(616,205))
 
 
-        font = ImageFont.truetype('Alice_in_Wonderland_3.ttf', 45)
-        print(font)                        
+        font = ImageFont.truetype('impacted.ttf', 45)
+                          
         draw.text((150,585), user_name,(251, 232, 255), font=font)
         draw.text((670,585), user2_name,(251, 232, 255), font=font)
 
