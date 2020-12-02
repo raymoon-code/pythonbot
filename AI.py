8a4aabebb1b19911dfd8e01bdd441cee64bb9c1f
raymoon-code <66543973+raymoon-code@users.noreply.github.com>
2020-12-02 01:05:39 -0600

    Update AI.py

--- AI.py
+++ AI.py
@@ -454,9 +454,9 @@ async def fight(ctx, user: discord.Member = None, user2: discord.Member = None):
         pfp2 = pfp2.resize((200,200))
         fight.paste(pfp,(81,223))
         fight.paste(pfp2,(703,223))
-        font = ImageFont.truetype('impact.ttf', 45)
-        draw.text((100,440), str(user_name),(251, 232, 255),  font=font)
-        draw.text((730,440), str(user2_name),(251, 232, 255),  font=font)
+#         font = ImageFont.truetype('arial.ttf', 45)
+#         draw.text((100,440), str(user_name),(251, 232, 255),  font=font)
+#         draw.text((730,440), str(user2_name),(251, 232, 255),  font=font)
     elif r == 'fight1.jpg':
         pfp = pfp.resize((295,239))
         pfp2 = pfp2.resize((295,239))
@@ -464,9 +464,9 @@ async def fight(ctx, user: discord.Member = None, user2: discord.Member = None):
         fight.paste(pfp2,(616,205))
 
 
-#         font = ImageFont.truetype('impact.ttf', 45)
-#         draw.text((150,585), user_name, (251, 232, 255), font=font)
-#         draw.text((670,585), user2_name,(251, 232, 255),  font=font)
+        font = ImageFont.truetype('arial.ttf', 45)
+        draw.text((150,585), user_name, (251, 232, 255), font=font)
+        draw.text((670,585), user2_name,(251, 232, 255),  font=font)
 
 
     fight.save('fighting.jpg')
